f7ef7eb65359c819cb085a78898b330e70c02fc1
DiegoAraujoJS <diegolaraujo96@gmail.com>
2022-12-24 18:24:51 -0300

    Testing go git

--- main/main.go
+++ main/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/go-git/go-git/v5"
+)
 
 func main() {
 	fmt.Println("Hello from main")
+	repo, err := git.PlainOpen(".git")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	logResult, err := repo.Log(&git.LogOptions{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	for {
+		l, err := logResult.Next()
+		if err != nil {
+			break
+		}
+		fmt.Println(l.String())
+	}
 }
