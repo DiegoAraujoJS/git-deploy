4055279dd7549fba9f90a0ee23f91d294969f8e3
Diego Araujo <diegolaraujo96@gmail.com>
2023-04-21 21:51:05 -0300

    Switch bashscript scripting for Python scripting, on build.

--- pkg/build-deploy/build.go
+++ pkg/build-deploy/build.go
@@ -9,12 +9,12 @@ import (
 
 // The function Build takes the name of the repo as a parameter. It executes the bash script located at the ./scripts folder, that has to be named as the repo with the "sh" extension. Example: for repo named "test", "test.sh".
 func Build(repo string) error {
-    script := "./scripts/" + repo + ".sh"
+    script := "./scripts/" + repo + ".py"
     if _, err := os.Stat(script); os.IsNotExist(err) {
         payload := fmt.Errorf("file does not exist", fmt.Sprint(err))
         return payload
 	}
-	cmd := exec.Command(script)
+	cmd := exec.Command("python", script)
     var out bytes.Buffer
     var stderr bytes.Buffer
     cmd.Stdout = &out
